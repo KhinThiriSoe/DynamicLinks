157fb0bf6c51f26785bb3ac03da4e9193c0420a5
Khin <khinthirisoe.mdy@gmail.com>
2019-11-14 12:04:54 +0700

    init commit

---
+++ app/src/main/java/com/khinthirisoe/dynamiclinks/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.khinthirisoe.dynamiclinks
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (intent != null) {
+            handleDeepLink(intent)
+        }
+    }
+
+    private fun handleDeepLink(intent: Intent) {
+        FirebaseDynamicLinks.getInstance()
+            .getDynamicLink(intent)
+            .addOnSuccessListener { pendingDynamicLinkData ->
+
+                if (pendingDynamicLinkData != null) {
+                    Log.d("message","getDynamicLink:onSuccess " + pendingDynamicLinkData.link.toString())
+                }
+            }
+            .addOnFailureListener {
+                Log.d("message","getDynamicLink:onFailure ", it.cause)
+            }
+    }
+}
